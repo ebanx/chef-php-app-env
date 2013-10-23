0639769472118707630a6cead3b537c144bba298
Ariel Patschiki <ariel@ebanx.com>
2013-10-23 16:50:45 -0200

    Meta change & initial README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chef PHP App Env
+This is a simple Chef Cookbook to create env.php files in apps directories.
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name              "php-app-env"
-maintainer        "Ariel Patschiki"
-maintainer_email  "arielpts@me.com"
+maintainer        "EBANX TECNOLOGIA DA INFORMACAO LTDA"
+maintainer_email  "ariel@ebanx.com"
 license           "Apache 2.0"
-description       "PHP application env vars file"
+description       "PHP Application ENV Vars"
 version           "1.0.0"
 
 depends "deploy"
